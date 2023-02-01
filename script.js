2e17663cffc7afd9c18aa502bd07815b3972b57a
Blaser993 <corradodepinto.design@gmail.com>
2023-02-01 16:54:46 +0100

    NEW FEAT const added

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 console.log("works!");
 
 
+const DISCOUNT_NO = 1;
+
+const DISCOUNT_UNDER = 0.2;
+
+const DISCOUNT_OVER = 0.4;
+
+
 var element = document.getElementById("calcola");
 
 element.addEventListener("click",
 
     function() {
+
+        
 
         console.log("ciao")
 
