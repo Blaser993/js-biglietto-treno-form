5fa372816bf01067f2eb8cae80342a02946379fd
Blaser993 <corradodepinto.design@gmail.com>
2023-02-01 19:43:26 +0100

    START Milestone 2

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ console.log("works!");
 const COST_PER_KM = 0.21;
 
 
-
-let input
-
-
 var element = document.getElementById("calcola");
 
 element.addEventListener("click",
@@ -15,22 +11,13 @@ element.addEventListener("click",
     function() {
 
         distance = document.getElementById("distance").value
-        console.log(distance);
 
         discount = document.getElementById("discount").value
-        console.log(discount);
 
         let billPrice = (distance * COST_PER_KM);
-
         let discountPrice = parseFloat(billPrice - (billPrice * discount)).toFixed(2);
 
-        console.log(billPrice + " €");
-        
-        console.log(discount);
-        console.log(discountPrice + " €");
-
-        document.getElementById("bill_price").innerHTML = (discountPrice);
-       
+        document.getElementById("bill_price").innerHTML = (discountPrice + " €");
 
     }
 
